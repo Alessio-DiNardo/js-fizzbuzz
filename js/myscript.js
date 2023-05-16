1dec3de9562c6af4de059da30900d00259d1d9c5
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-05-16 15:26:12 +0200

    add comment

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,8 @@
+// DEVO SOSTITUIRE TUTTI I NUMERI MULTIPLI SIA DI 3 E 5 CON FIZZBUZZ
+// DEVO SOSTITUIRE TUTTI I NUMERI MULTIPLI DI 3 CON FIZZ
+// DEVO SOSTITUIRE TUTTI I NUMERI MULTIPLI DI 5 CON BUZZ
+
+
 for (let i = 0; i < 100; i++){
     if(i % 3 === 0 && i % 5 === 0) console.log("FizzBuzz");
     else if(i % 3 === 0) console.log("Fizz");

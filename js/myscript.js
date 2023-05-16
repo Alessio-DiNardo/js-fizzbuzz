57b865795adb93ff2937a8aea51875e85981606c
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-05-16 15:22:32 +0200

    Try to ins fizz buzz

---
+++ js/myscript.js
@@ -0,0 +1,8 @@
+for (let i = 0; i < 100; i++){
+    if(i % 3 === 0 && i % 5 === 0) console.log("FizzBuzz");
+    else if(i % 3 === 0) console.log("Fizz");
+    else if(i % 5 === 0) console.log("Buzz");
+    const FizzBuzz = "FizzBuzz"
+    const Fizz = "Fizz"
+    const Buzz = "Buzz"
+}
